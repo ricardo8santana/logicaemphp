42f42ba0a9eca41ebf481f27abb5f9feb6fe08cf
ricardo8santana <luisriccardo@proton.me>
2025-12-10 14:36:13 -0300

    inicializando com loop

--- aula05-loop/index.php
+++ aula05-loop/index.php
@@ -3,11 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Loop</title>
+    <title>Loop, While, Do, For, Foreach</title>
 </head>
 <body>
     <?php 
-        
+        //while
+        $i = 1;
+        while($i <= 5){
+            echo $i . "<br>"; //Imprimi o valçor de $i e pula a próxima linha
+            $i++;
+        }
     
     ?>
 </body>
